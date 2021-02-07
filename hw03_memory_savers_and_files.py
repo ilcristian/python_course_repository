0aae08b387eb3e8f65036199b49aea1ac88f1208
CristianLI <cristianlozba@gmail.com>
2021-02-07 23:05:11 +0200

    hw3

---
+++ hw03_memory_savers_and_files.py
@@ -0,0 +1,63 @@
+import csv
+import json
+
+header = ()
+car_details = []
+
+while True:
+    filename = input('Enter a filename (enter \'q\' to exit): ')
+    if filename == 'q':
+        break
+    else:
+        try:
+            with open(filename) as file:
+                csv_data = csv.reader(file)
+                for index, data in enumerate(csv_data):
+                    if index == 0:
+                        header = data
+                    else:
+                        car_details.append(data)
+            break
+        except FileNotFoundError:
+            print('The file doesn\'t exist!')
+            continue
+
+cars = [{key: value for key, value in zip((head.strip() for head in header), (c.strip() for c in car))}
+        for car in car_details]
+
+slow_cars = [slow for slow in cars if int(slow['hp(horse power)']) < 120]
+fast_cars = [fast for fast in cars if 120 <= int(fast['hp(horse power)']) < 180]
+sport_cars = [sport for sport in cars if int(sport['hp(horse power)']) >= 180]
+
+with open('slow_cars.json', 'w') as json_slow_cars, \
+        open('fast_cars.json', 'w') as json_fast_cars, \
+        open('sport_cars.json', 'w') as json_sport_cars:
+
+    slow_cars_file = json.dumps(slow_cars)
+    json_slow_cars.write(slow_cars_file)
+
+    fast_cars_file = json.dumps(fast_cars)
+    json_fast_cars.write(fast_cars_file)
+
+    sport_cars_file = json.dumps(sport_cars)
+    json_sport_cars.write(sport_cars_file)
+
+
+cheap_cars = [cheap for cheap in cars if int(cheap['price']) < 1000]
+medium_cars = [medium for medium in cars if 1000 <= int(medium['price']) < 5000]
+expensive_cars = [expensive for expensive in cars if int(expensive['price']) >= 5000]
+
+with open('cheap_cars.json', 'w') as json_cheap_cars, \
+        open('medium_cars.json', 'w') as json_medium_cars, \
+        open('expensive_cars.json', 'w') as json_expensive_cars:
+
+    cheap_cars_file = json.dumps(cheap_cars)
+    json_cheap_cars.write(cheap_cars_file)
+
+    medium_cars_file = json.dumps(medium_cars)
+    json_medium_cars.write(medium_cars_file)
+
+    expensive_cars_file = json.dumps(expensive_cars)
+    json_expensive_cars.write(expensive_cars_file)
+
+
